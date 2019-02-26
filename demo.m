217774bf790eaceedb947be6abc049567fefb371
CHEN <chinn.shikou@gmail.com>
2019-02-26 17:06:41 +0900

    first commit

---
+++ demo.m
@@ -0,0 +1,20 @@
+clc;clear;close all;
+
+addpath('./flann/');
+addpath('./estimateRigidTransform');
+
+gridStep = 0.01;
+Rho = 0.01;
+datapath = './data';
+
+srcFileName = 'data/bun000.ply';
+tarFileName = 'data/bun090.ply';
+srcCloud = pcread(srcFileName);
+tarCloud = pcread(tarFileName);
+overlap = 0.5;
+tic;
+[T,src2tarEst] = eigReg(srcCloud,tarCloud,overlap,gridStep,Rho);
+Time = toc
+figure,pcshow(tarCloud.Location,'r'),hold on
+pcshow(src2tarEst','g');hold on
+pcshow(srcCloud.Location,'b')
